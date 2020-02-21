ab9f03a2ee639372a8e498e44104278c239b47e1
Gaurav <gbaney@usc.edu>
2020-02-20 16:43:10 -0800

    euclidean distance done

--- HW2/kmeans.py
+++ HW2/kmeans.py
@@ -1,4 +1,5 @@
 import numpy as npy
+import pprint as pp
 from random import sample
 
 #https://www.analyticsvidhya.com/blog/2019/08/comprehensive-guide-k-means-clustering/
@@ -9,6 +10,9 @@ class Point:
     def __init__(self, x, y):
         self.x = x
         self.y = y
+
+    def distance(self, other):
+        return npy.sqrt((((self.x - other.x) * (self.x - other.x)) + ((self.y - other.y) * (self.y - other.y))))
 
     def __repr__(self):
         return ("(X: " + str(self.x) + ", Y: " + str(self.y) + ")")
@@ -27,19 +31,34 @@ class kmeans:
 
         return
 
+    def assignClusters(self, data, centroids):
+        currMap = {c : [] for c in centroids}
+        for point in data:
+            minDist = 10000000
+            cc = None
+            for centroid in centroids:
+                dist = point.distance(centroid)
+                if dist < minDist:
+                    minDist = dist
+                    cc = centroid
+            currMap[cc].append(point)
+
+        return currMap
+
     def start(self):
-        initCentroids = npy.random.choice(self.points, 3)
-        
+        currCentroids = npy.random.choice(self.points, 3)
+        mainmap = None
         ite = 0
         difference = 10000
         while difference != 0 and ite < self.maxIterations:
             currData = self.points
+            clusters = self.assignClusters(currData, currCentroids)
+            currCentroids = self.reCalcCentroids(clusters)
+
             ite += 1
-            
+            mainmap = clusters
 
-        
-
-
+        return mainmap
 
     def parseDoc(self, filename):
         f = open(filename, 'r')
@@ -50,4 +69,4 @@ class kmeans:
         
         return npy.array(pointList)
 
-km = kmeans("clusters.txt", 3, 100)
+km = kmeans("clusters.txt", 3, 1)
