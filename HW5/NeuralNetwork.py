8f59f408e9d29b5bf4ff7e592fed66dcd6d4f433
Gaurav <gbaney@usc.edu>
2020-04-08 15:40:07 -0700

    hmm

--- HW5/NeuralNetwork.py
+++ HW5/NeuralNetwork.py
@@ -1,8 +1,14 @@
+import numpy as np 
+import pprint as pp
+
 class NeuralNetwork:
 
     def __init__(self, trainFile, testFile):
         self.trainData = self.parse(trainFile)
         self.testData = self.parse(testFile)
+        self.numLayers = 100
+        self.weights = {}
+        
     
     def parse(self, file):
         pass
