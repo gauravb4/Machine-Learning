24ea1518db7dcdc5630949f97527cb1446bdc840
Gaurav <gbaney@usc.edu>
2020-04-08 15:30:25 -0700

    parse skeleton

--- HW5/NeuralNetwork.py
+++ HW5/NeuralNetwork.py
@@ -1,4 +1,13 @@
 class NeuralNetwork:
 
-    def parse(self):
+    def __init__(self, trainFile, testFile):
+        self.trainData = self.parse(trainFile)
+        self.testData = self.parse(testFile)
+    
+    def parse(self, file):
         pass
+
+
+test = "downgesture_test.list"
+train = "downgesture_train.list"
+nn = NeuralNetwork(train, test)    
