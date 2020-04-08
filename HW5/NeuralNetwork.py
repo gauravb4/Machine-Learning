a13bfef0abfbf4bc307c146265c8b56592b756c9
Hans (Wei-Han) Chang <weihanc@usc.edu>
2020-04-08 16:20:10 -0700

    added sig functions

--- HW5/NeuralNetwork.py
+++ HW5/NeuralNetwork.py
@@ -7,18 +7,21 @@ class NeuralNetwork:
         self.trainData = self.parse(trainFile)
         self.testData = self.parse(testFile)
         self.numLayers = 100
-        self.weights = {}
-        
+        self.weights = {}    
     
     def parse(self, file):
         file_list = []
-
         with open(file, 'r') as f:
             for line in f:
-                file_list.append(line.strip())
-        return file_list
+                label = 1 if "down" in line else 0
+                file_list.append([line.strip(), label])
+        return np.array(file_list)
 
+    def sig(self, s):
+        return 1/(1+np.exp(-s))
 
+    def sig_b(self, s):
+        return sig(s) * (1 - sig(s))
 
 test = "downgesture_test.list"
 train = "downgesture_train.list"
