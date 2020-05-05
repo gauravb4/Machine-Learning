ddc39f894bcb894aa5d5649421d09a01c251226c
Gaurav <gbaney@usc.edu>
2020-05-05 04:39:56 -0700

    added result animation

--- HW7/hmm.py
+++ HW7/hmm.py
@@ -2,6 +2,13 @@ import numpy as np
 import pprint as pp
 from collections import defaultdict
 import copy
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import matplotlib.axes
+import matplotlib.animation as animation
+from matplotlib.animation import PillowWriter
+from PIL import Image
+import glob
 
 
 class PathNode:
@@ -57,7 +64,17 @@ class Point:
         self.y = y
         self.obstacle = True if obstacle == 0 else False
         self.gridSize = gridSize
-        self.neighbors = self.findNeighbors()
+        self.n, self.s, self.w, self.e,self.neighbors = self.findNeighbors()
+        
+    def direction(self, other):
+        if self.n == other:
+            return "V"
+        if self.s == other:
+            return "^"
+        if self.w == other:
+            return ">"
+        if self.e == other:
+            return "<"
 
     def equal(self, other):
         return (self.x == other.x and self.y == other.y)
@@ -76,25 +93,28 @@ class Point:
 
     def findNeighbors(self):
         n = []
+        n1 = None
+        n2 = None
+        n3 = None
+        n4 = None
         if(self.x - 1 >= 0):
-            n.append((self.x - 1, self.y))
+            n1 = (self.x - 1, self.y)
+            n.append(n1)
         if(self.x + 1 < self.gridSize):
-            n.append((self.x + 1, self.y))
+            n2 = (self.x + 1, self.y)
+            n.append(n2)
         if(self.y - 1 >= 0):
-            n.append((self.x, self.y - 1))
+            n3 = (self.x, self.y - 1)
+            n.append(n3)
         if(self.y + 1 < self.gridSize):
-            n.append((self.x, self.y + 1))
-        return n
+            n4 = (self.x, self.y + 1)
+            n.append(n4)
+        return n1, n2, n3, n4, n
 
 class HMM:
 
     def __init__(self, filename):
         self.grid, towers, distances = self.parse(filename)
-        self.states = []
-        for row in self.grid.grid:
-            for point in row:
-                if not point.obstacle:
-                    self.states.append((point))
         towerDistances  = self.calcDistances(towers)
         self.getNextStates(distances, towerDistances)
         self.tProb = self.transition()
@@ -107,7 +127,113 @@ class HMM:
         res = []
         for node in result:
             res.append(node.point)
-        pp.pp(res)
+        grid = copy.deepcopy(self.grid.grid)
+        for i in range(10):
+            for j in range(10):
+                if(grid[i][j].isObstacle()):
+                    grid[i][j] = "0"
+                else:
+                    grid[i][j] = "1"
+        grid[res[0].x][res[0].y] = "S"
+        last = (res[0].x, res[0].y)
+        for p in res[1:-1]:
+            grid[p.x][p.y] = p.direction(last)
+            last = (p.x, p.y)
+        grid[res[-1].x][res[-1].y] = "G"
+        pp.pp(grid)
+        self.drawResult(grid, res)
+
+    def drawResult(self, grid, res):
+        x = [i for i in range(-2, 12, 1)]
+        y = [i for i in range(-2, 12, 1)]
+
+        cmap = mpl.colors.ListedColormap(['black','white'])
+        
+        zvals = copy.deepcopy(grid)
+        for i in range(10):
+            for j in range(10):
+                if zvals[i][j] == "0":
+                    zvals[i][j] = float(-5) 
+                else:
+                    zvals[i][j] = float(5)
+
+        bounds=[-10,0,10]
+        norm = mpl.colors.BoundaryNorm(bounds, cmap.N)
+        fig = plt.figure()
+        img = plt.imshow(zvals,interpolation='nearest', cmap = cmap,norm=norm)
+        
+        ax = plt.gca();
+        ax.set_xticks(np.arange(-.5, 10, 1))
+        ax.set_yticks(np.arange(-.5, 10, 1))
+        ax.set_xticklabels(np.arange(0, 10, 1), )
+        ax.set_yticklabels(np.arange(0, 10, 1))
+        plt.grid()
+        for label in ax.get_xticklabels():
+            label.set_horizontalalignment('center')
+
+        list1 = ax.get_xticklabels()
+        plt.savefig('0')
+        plt.savefig('1')
+
+        frame = 0
+        
+        for frame in range(11):
+            self.addMoreImages(grid, res, frame)
+            
+        plt.savefig('13')
+      
+        self.saveGif()    
+
+    def addMoreImages(self, grid, res, maxR):
+
+        cmap = mpl.colors.ListedColormap(['black','white', 'Green', 'Red'])
+        
+        zvals = copy.deepcopy(grid)
+        for i in range(10):
+            for j in range(10):
+                if zvals[i][j] == "0":
+                    zvals[i][j] = float(-9) 
+                else:
+                    zvals[i][j] = float(-2)
+
+        for p in res[0:maxR]:
+            zvals[p.x][p.y] = float(2)
+
+        if maxR == 10:
+            p = res[-1]
+            zvals[p.x][p.y] = float(7)
+
+        bounds=[-10,-5,0,5,10]
+        norm = mpl.colors.BoundaryNorm(bounds, cmap.N)
+
+        img2 = plt.imshow(zvals,interpolation='None', cmap = cmap,norm=norm)
+        
+        ax = plt.gca();
+        ax.set_xticks(np.arange(-.5, 10, 1))
+        ax.set_yticks(np.arange(-.5, 10, 1))
+        ax.set_xticklabels(np.arange(0, 10, 1), )
+        ax.set_yticklabels(np.arange(0, 10, 1))
+
+        for label in ax.get_xticklabels():
+            label.set_horizontalalignment('center')
+
+        list1 = ax.get_xticklabels()
+        plt.savefig('{}'.format(maxR + 2))
+
+    def saveGif(self):
+        frames = []
+        imgs = []
+        for i in range(14):
+            imgs.append(glob.glob("{}.png".format(i)))
+        for i in imgs:
+            new_frame = Image.open(i[0])
+            frames.append(new_frame)
+        
+        # Save into a GIF file that loops forever
+        frames[0].save('route.gif', format='GIF',
+                    append_images=frames[1:],
+                    save_all=True,
+                    duration=100, loop=10000)
 
     def generatePath(self, allPaths):
         mProb = 0.0
@@ -146,6 +272,7 @@ class HMM:
                         curr = PathNode(n, node, prob, times)
                         currTimeNodes.append(curr)
                     else:
+                        #curr.parent = node
                         prob = node.probability * 0.25
                         curr.probability += prob
             paths.append(currTimeNodes)            
@@ -236,9 +363,11 @@ class HMM:
         timesorted = []
         for timestep, distance in enumerate(distances):
             timesorted.append(set())
-            for index, state_distances in enumerate(towerDistances):
-                if self.Possible(state_distances, distance):
-                    timesorted[timestep].add(self.states[index])
+            for index, pointDistances in enumerate(towerDistances):
+                if pointDistances != "Empty" and self.Possible(pointDistances, distance):
+                    x  = int(index / 10)
+                    y = int(index % 10)
+                    timesorted[timestep].add(self.grid.grid[x][y])
         self.pointsListByTime = timesorted
 
     def Possible(self, state, distance):
@@ -256,8 +385,8 @@ class HMM:
                     for tower in towers:
                         t.append(point.distance(tower))
                     distances.append(tuple(t))
-                #else:
-                #    distances.append("Empty")
+                else:
+                    distances.append("Empty")
         return distances
 
     def parse(self, filename):
@@ -272,6 +401,7 @@ class HMM:
         for i in range(gridSize):
             l = d1[i].split(" ")
             for j in range(gridSize):
+                #print("x and y {} {} and value {}".format(i, j, l[j]))
                 g.grid[i][j] = Point(i, j, int(l[j]), gridSize)
 
         towers = []
